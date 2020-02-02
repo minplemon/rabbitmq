8f2652aa549fc0ab01c6f186b1b88b4421a6815c
minp <king101125s@gmail.com>
2020-02-02 14:54:38 +0800

    provider消息推送实例，consumer消息消费实例，Direct、Topic、Fanout的使用，消息回调、手动确认等

---
+++ rabbitmq-provider/src/test/java/org/minp/rabbitmqprovider/RabbitmqProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.minp.rabbitmqprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
